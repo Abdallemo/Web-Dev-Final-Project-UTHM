781eb15f1a1e3fac95b149650d361d4ec422502b
EngCamo <learn3038it@gmail.com>
2024-06-07 04:50:12 +0800

    added express js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,37 @@
-console.log("this is the first nodejs ")
-console.log("second line ")
+const express = require('express')
+const app = express()
+const tutorialRouter = require('../backend/router/tutorialRouter')
+const path = require('path')
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, '../frontend/views'))
+
+app.use('/tutorials',tutorialRouter)
+
+
+app.get('/',(req,res)=>
+    {
+        const tutorials =
+        [
+            {
+                title: 'Test tutorial 1',
+                createAt: new Date(),
+                description: 'this the first tutorial'
+
+            },
+            {
+                title: 'Test tutorial 2',
+                createAt: new Date(),
+                description: 'this the second tutorial'
+
+            }
+        ]
+        res.render('index',{tutorials:tutorials})
+    })
+
+
+app.listen(5000)
+
+
+
+
+
