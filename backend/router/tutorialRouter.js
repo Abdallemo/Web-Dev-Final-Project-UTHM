f8ca83489b4e2dcb2e90f4937b5cfe638ca45858
EngCamo <learn3038it@gmail.com>
2024-06-07 07:12:52 +0800

    updated router

--- backend/router/tutorialRouter.js
+++ backend/router/tutorialRouter.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Tutorial = require('./../models/toturialModel')
 
 router.get('/new',(req, res)=>
     {
         res.render('tutorials/new')
     })
-
-router.post('/',(req,res)=>
+router.get('/:id',(req,res)=>
     {
-        
+
+    })    
+
+router.post('/', async (req,res)=>
+    {
+        const tutorial = new Tutorial(
+            {
+                title: req.body.title,
+                Description: req.body.Description,
+                markdown: req.body.markdown,
+            })
+            try
+            {
+                tutorial = await tutorial.save()
+                res.redirect(`/tutorials/${tutorial.id}`)
+            }catch(e)
+            {
+                res.render('tutorials/new',{tutorial:tutorial})
+            }
     })
 
 module.exports = router
