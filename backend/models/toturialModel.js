322dc0161885256ec74fbfa6c7b629d55228ac74
EngCamo <learn3038it@gmail.com>
2024-06-07 08:51:26 +0800

    some major updates

--- backend/models/toturialModel.js
+++ backend/models/toturialModel.js
@@ -2,6 +2,10 @@ const { type } = require('express/lib/response')
 const mongoose = require('mongoose')
 const { describe } = require('node:test')
 const { title } = require('process')
+const marked = require('marked')
+const slugify = require('slugify')
+const { strict } = require('assert')
+
 
 const tutorialschema = new mongoose.Schema(
     {
@@ -10,7 +14,7 @@ const tutorialschema = new mongoose.Schema(
             type:String,
             required:true
         },
-        descrpition:
+        Description:
         {
             type:String
         },
@@ -23,6 +27,24 @@ const tutorialschema = new mongoose.Schema(
         {
             type: Date,
             default:Date.now
+        },
+        slug:
+        {
+            type:String,
+            required:true,
+            unique:true
         }
+    })
+    tutorialschema.pre('validate',function(next)
+    {
+        if(this.title)
+            {
+                this.slug= slugify(this.title,
+                    {
+                        lower:true,strict:true
+                    }
+                )
+            }
+            next()
     })
     module.exports= mongoose.model('Tutorial',tutorialschema)
--- backend/router/tutorialRouter.js
+++ backend/router/tutorialRouter.js
@@ -4,16 +4,19 @@ const Tutorial = require('./../models/toturialModel')
 
 router.get('/new',(req, res)=>
     {
-        res.render('tutorials/new')
+        res.render('tutorials/new',{tutorial:new Tutorial()})
     })
-router.get('/:id',(req,res)=>
+router.get('/:slug', async (req,res)=>
     {
+        const tutorial= await Tutorial.findOne( {slug: req.params.slug})
+        if(tutorial==null) res.redirect('/')
+        res.render('tutorials/show',{tutorial:tutorial})
 
     })    
 
 router.post('/', async (req,res)=>
     {
-        const tutorial = new Tutorial(
+        let tutorial = new Tutorial(
             {
                 title: req.body.title,
                 Description: req.body.Description,
@@ -22,11 +25,18 @@ router.post('/', async (req,res)=>
             try
             {
                 tutorial = await tutorial.save()
-                res.redirect(`/tutorials/${tutorial.id}`)
+                res.redirect(`/tutorials/${tutorial.slug}`)
             }catch(e)
             {
+                console.log(e)
                 res.render('tutorials/new',{tutorial:tutorial})
             }
+    })
+
+router.delete('/:id',async (req,res)=>
+    {
+        await Tutorial.findByIdAndDelete(req.params.id)
+        res.redirect('/')
     })
 
 module.exports = router
