2a5ed039ff464202abe31db52e57d73ecb4f8fba
EngCamo <learn3038it@gmail.com>
2024-06-07 11:31:35 +0800

    chnaged i dont know!!

--- backend/models/toturialModel.js
+++ backend/models/toturialModel.js
@@ -8,6 +8,44 @@ const { strict } = require('assert')
 const createDompurify = require('dompurify')
 const {JSDOM} = require('jsdom')
 const dompurify = createDompurify(new JSDOM().window)
+const hljs = require('highlight.js');
+
+marked.setOptions({
+    gfm: true,
+    breaks: true,
+    tables: true,
+    sanitize: false, // This is not a recommended setting, as it might introduce XSS vulnerabilities. Use with caution.
+    highlight: function (code, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            return hljs.highlight(lang, code).value;
+        } else {
+            return hljs.highlightAuto(code).value;
+        }
+    }
+  });
+
+  // Add hooks to dompurify for HTML sanitization
+dompurify.addHook('afterSanitizeAttributes', function(node) {
+    // Whitelist attributes for <code> elements
+    if ('nodeName' in node && node.nodeName.toLowerCase() === 'code') {
+        node.setAttribute('class', 'language-markup'); // Example: Add class for syntax highlighting
+        // Add more allowed attributes if necessary
+    }
+});
+
+dompurify.addHook('afterSanitizeElements', function(node) {
+    // Whitelist elements
+    if ('nodeName' in node) {
+        const nodeName = node.nodeName.toLowerCase();
+        if (nodeName === 'pre' || nodeName === 'code') {
+            // Allow <pre> and <code> elements
+            // Add more allowed elements if necessary
+        }
+    }
+});
+
+
+
 
 
 const tutorialschema = new mongoose.Schema(
